236e2ea8845304834b3815695b4ebc85ddf65abf
jbusomm <145897540+jbusomm@users.noreply.github.com>
2023-12-08 12:24:11 +0100

    Add 16x16 grid of 1px square divs

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-for (i = 0; i < 256; i++) {
+for (i = 0; i <= 255; i++) {
   const container = document.querySelector(".container");
   const square = document.createElement("div");
-  square.style.width = "8px";
-  square.style.height = "8px";
+  square.style.width = "1px";
+  square.style.height = "1px";
   square.style.background = "red";
-  square.style.color = "white";
-  square.style.display = "flex";
-  square.classList.add("square-div");
   container.appendChild(square);
 }
 
