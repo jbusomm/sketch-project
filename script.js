75a85e4087015d1a977ef838163e3adc286e7d4e
jbusomm <145897540+jbusomm@users.noreply.github.com>
2023-12-08 12:39:06 +0100

    Make 16x16 canvas bigger with 16x16 pixels

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 for (i = 0; i <= 255; i++) {
   const container = document.querySelector(".container");
   const square = document.createElement("div");
-  square.style.width = "1px";
-  square.style.height = "1px";
+  square.style.width = "16px";
+  square.style.height = "16px";
   square.style.background = "red";
   container.appendChild(square);
 }
