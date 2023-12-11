1f33ba8f8e0ce422e8e300880a40f67d76e87288
jbusomm <145897540+jbusomm@users.noreply.github.com>
2023-12-11 10:34:58 +0100

    Add mouse click paint functionality and UI touches

--- script.js
+++ script.js
@@ -3,8 +3,13 @@ for (i = 0; i <= 255; i++) {
   const square = document.createElement("div");
   square.style.width = "16px";
   square.style.height = "16px";
-  square.style.background = "red";
+  square.style.background = "white";
   container.appendChild(square);
+  square.addEventListener("mouseover", function (e) {
+    if (e.buttons == 1) {
+      square.style.backgroundColor = "black";
+    }
+  });
 }
 
 console.log("Hello World!");
