53e8ed30bbd0bd963aac434e442c6e0b21791c55
jbusomm <145897540+jbusomm@users.noreply.github.com>
2023-12-11 12:37:05 +0100

    Revamp how the canvas gets generated and thus easier to change size

--- script.js
+++ script.js
@@ -1,15 +1,42 @@
-for (i = 0; i <= 255; i++) {
-  const container = document.querySelector(".container");
-  const square = document.createElement("div");
-  square.style.width = "16px";
-  square.style.height = "16px";
-  square.style.background = "white";
-  container.appendChild(square);
-  square.addEventListener("mouseover", function (e) {
-    if (e.buttons == 1) {
+// for (i = 0; i <= 255; i++) {
+//   const container = document.querySelector(".container");
+//   const square = document.createElement("div");
+//   square.style.width = "32px";
+//   square.style.height = "32px";
+//   square.style.background = "white";
+//   container.appendChild(square);
+//   square.addEventListener("mousedown", (e) => {
+//     square.style.backgroundColor = "black";
+//   });
+//   square.addEventListener("mouseover", function (e) {
+//     if (e.buttons == 1) {
+//       square.style.backgroundColor = "black";
+//     }
+//   });
+// }
+
+const container = document.querySelector(".container");
+function canvasGen(x) {
+  totalPixels = x * x - 1;
+  canvasPixels = 8 * x;
+  for (i = 0; i <= totalPixels; i++) {
+    const square = document.createElement("div");
+    square.style.width = "8px";
+    square.style.height = "8px";
+    square.style.backgroundColor = "white";
+    container.appendChild(square);
+    container.style.width = canvasPixels + "px";
+    container.style.height = canvasPixels + "px";
+    square.addEventListener("mousedown", (e) => {
       square.style.backgroundColor = "black";
-    }
-  });
+    });
+    square.addEventListener("mouseover", function (e) {
+      if (e.buttons == 1) {
+        square.style.backgroundColor = "black";
+      }
+    });
+  }
 }
+canvasGen(50);
 
 console.log("Hello World!");
